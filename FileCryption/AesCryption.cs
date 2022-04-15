0f823d756d90aa4c8ceec71027abd06b9c9c8964
Ryan Amos <amos2580@hotmail.co.uk>
2022-04-15 22:21:53 +0100

    Boilerplate Init Commit

---
+++ FileCryption/AesCryption.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace FileEncryption;
+
+public static class AesCryption
+{
+    public static string Encrypt(string text, string iv, string key)
+    {
+        var cipher = KeyConversion.CreateCipher(key);
+        cipher.IV = Convert.FromBase64String(iv);
+ 
+        var cryptTransform = cipher.CreateEncryptor();
+        var plaintext = Encoding.UTF8.GetBytes(text);
+        var cipherText = cryptTransform.TransformFinalBlock(plaintext, 0, plaintext.Length);
+ 
+        return Convert.ToBase64String(cipherText);
+    }
+
+    public static string Decrypt(string encryptedText, string iv, string key)
+    {
+        var cipher = KeyConversion.CreateCipher(key);
+        cipher.IV = Convert.FromBase64String(iv);
+ 
+        var cryptTransform = cipher.CreateDecryptor();
+        var encryptedBytes = Convert.FromBase64String(encryptedText);
+        var plainBytes = cryptTransform.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+ 
+        return Encoding.UTF8.GetString(plainBytes);
+    }
+}
---
+++ FileCryption/KeyConversion.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+
+namespace FileEncryption;
+
+public static class KeyConversion
+{
+    public static (string Key, string IVBase64) SymmetricEncryptDecrypt()
+    {
+        var key = GetEncodedRandomString(32); // 256
+        var cipher = CreateCipher(key);
+        var ivBase64 = Convert.ToBase64String(cipher.IV);
+        return (key, ivBase64);
+    }
+ 
+    private static string GetEncodedRandomString(int length)
+    {
+        var base64 = Convert.ToBase64String(GenerateRandomBytes(length));
+        return base64;
+    }
+ 
+    public static Aes CreateCipher(string keyBase64)
+    {
+        // Default values: Keysize 256, Padding PKC27
+        var cipher = Aes.Create();
+        cipher.Mode = CipherMode.CBC;  // Ensure the integrity of the ciphertext if using CBC
+ 
+        cipher.Padding = PaddingMode.ISO10126;
+        cipher.Key = Convert.FromBase64String(keyBase64);
+ 
+        return cipher;
+    }
+ 
+    private static byte[] GenerateRandomBytes(int length)
+    {
+        var byteArray = new byte[length];
+        RandomNumberGenerator.Fill(byteArray);
+        return byteArray;
+    }
+}
